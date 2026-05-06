c0f74e519770c412f16042dd5c3199882f9d0e0d
SuperDedСемён1942 <semen52.crow@mail.ru>
2026-05-06 18:42:11 +0300

    Update player.js

--- player.js
+++ player.js
@@ -24,3 +24,20 @@ updateMovement() {
       this.y = newY;
   }
 }
+draw(ctx) {
+    ctx.fillStyle = '#ffaa44';
+    ctx.beginPath();
+    ctx.arc(this.x + this.size / 2, this.y + this.size / 2, 8, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.fillStyle = '#ffffff';
+    ctx.beginPath();
+    ctx.arc(this.x + this.size / 2 - 2, this.y + this.size / 2 - 2, 2, 0, Math.PI * 2);
+    ctx.fill();
+    const keys = window.InputState.keys;
+    ctx.fillStyle = '#ff6600';
+
+    if (keys.w) ctx.fillRect(this.x + 15, this.y + this.size + 2, 10, 8);
+    if (keys.s) ctx.fillRect(this.x + 15, this.y - 8, 10, 8);
+    if (keys.a) ctx.fillRect(this.x + this.size + 2, this.y + 15, 8, 10);
+    if (keys.d) ctx.fillRect(this.x - 8, this.y + 15, 8, 10);
+}
