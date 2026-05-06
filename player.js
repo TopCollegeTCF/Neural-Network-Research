a821cc76b07595f7f3bf49eb1bd721e7e1ac1dda
t1mch1k12 <kim228455@gmail.com>
2026-05-06 18:39:24 +0300

    Update player.js

--- player.js
+++ player.js
@@ -7,3 +7,20 @@ class Player{
   }
 }
 window.Player = new Player();
+
+updateMovement() {
+  let newX = this.x;
+  let newY = this.y;
+  const keys = window.InputState.keys;
+  if (keys.w) newY -= this.speed;
+  if (keys.s) newY += this.speed;
+  if (keys.a) newX -= this.speed;
+  if (keys.d) newX += this.speed;
+
+  if (newX >= CONFIG.bounds.xMin && newX <= CONFIG.bounds.xMax) {
+      this.x = newX;
+  }
+  if (newY >= CONFIG.bounds.yMin && newY <= CONFIG.bounds.yMax) {
+      this.y = newY;
+  }
+}
