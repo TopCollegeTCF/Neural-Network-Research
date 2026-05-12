93a1389d3ddf851cebb221251d2471040c7ef5f4
scalaragg <dlaeve321@gmail.com>
2026-05-12 17:21:24 +0300

    Update state.js

--- state.js
+++ state.js
@@ -1,44 +1,50 @@
-let animationId = null;
-let isRunning = true;
+// state.js - Состояние игры и цикл обновления
 
-function initState() {
-    console.log('state.js инициализирован');
-    startGameLoop();
-}
+// Основной объект состояния игры
+const GameState = {
+    // Состояние клавиш (WASD)
+    keys: {
+        w: false,
+        s: false,
+        a: false,
+        d: false
+    },
 
-function gameLoop() {
-    if (!isRunning) return;
+    // Позиция и размеры игрока
+    player: {
+        x: 400,
+        y: 300,
+        width: 40,
+        height: 40
+    },
 
-    if (window.Player && typeof window.Player.updateMovement === 'function') {
-        window.Player.updateMovement();
+    // Счёт игрока
+    score: 0,
+
+    // Скорость перемещения игрока (можно настраивать)
+    playerSpeed: 5,
+
+    /**
+     * Метод обновления логики игры (вызывается каждый кадр)
+     */
+    update() {
+        // Перемещение игрока по WASD
+        if (this.keys['w'] && this.player.y > 0) {
+            this.player.y -= this.playerSpeed;
+        }
+        if (this.keys['s'] && this.player.y < 600 - this.player.height) { // предполагаем высоту canvas 600
+            this.player.y += this.playerSpeed;
+        }
+        if (this.keys['a'] && this.player.x > 0) {
+            this.player.x -= this.playerSpeed;
+        }
+        if (this.keys['d'] && this.player.x < 800 - this.player.width) { // предполагаем ширину canvas 800
+            this.player.x += this.playerSpeed;
+        }
     }
+};
 
-    if (window.render && typeof window.render === 'function') {
-        window.render();
-    }
+// Экспортируем GameState в глобальную область (window)
+window.GameState = GameState;
 
-    animationId = requestAnimationFrame(gameLoop);
-}
-
-function startGameLoop() {
-    if (animationId) {
-        cancelAnimationFrame(animationId);
-    }
-
-    isRunning = true;
-    console.log("Игровой цикл запущен");
-    animationId = requestAnimationFrame(gameLoop);
-}
-
-function stopGameLoop() {
-    isRunning = false;
-    if (animationId) {
-        cancelAnimationFrame(animationId);
-        animationId = null;
-    }
-    console.log("Игровой цикл остановлен");
-}
-
-window.initState = initState;
-window.stopGameLoop = stopGameLoop;
-window.startGameLoop = startGameLoop;
+export default GameState;
